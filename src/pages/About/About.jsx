9dcc7332c1aa32cbba8b6801555cea4d48b71dbc
vertpierre <74794864+vertpierre@users.noreply.github.com>
2024-06-23 21:03:25 +0200

    feat(about): implement Collapse component and About page
    - Create Collapse component with open/close functionality and props for flexible usage
    - Implement About page using Collapse components
    - Correction of data loading to load data from routes in App, and share it through props, Home corrected accordingly

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,47 @@
+import styles from './About.module.scss';
+import Banner from '../../components/Banner/Banner';
+import Collapse from '../../components/Collapse/Collapse';
+
+const aboutList = {
+    fiability: [
+        'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+    ],
+    respect: [
+        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+    ],
+    service: [
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    ],
+    safety: [
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    ],
+};
+
 const About = () => {
+    const getTitleFromKey = (key) => {
+        const titleMap = {
+            fiability: 'Fiabilité',
+            respect: 'Respect',
+            service: 'Service',
+            safety: 'Sécurité',
+        };
+        return titleMap[key] || key;
+    };
+
     return (
-        <div>
-            <h1>A propos</h1>
+        <div className={styles.about}>
+            <Banner
+                title=""
+                image="src/assets/images/about.webp"
+                opacity={0.7}
+            />
+            {Object.entries(aboutList).map(([key, value]) => (
+                <Collapse
+                    key={key}
+                    title={getTitleFromKey(key)}
+                    content={value}
+                />
+            ))}
         </div>
     );
 };
